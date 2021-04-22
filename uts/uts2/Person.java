4675394f14495722aae3c840c97a38dfcc3102c9
Intan Nas Nas Rabbiana <80866422+IntanNasNasRabbiana@users.noreply.github.com>
2021-04-22 08:58:42 +0700

    Add files via upload

---
+++ uts/uts2/Person.java
@@ -0,0 +1,17 @@
+/*Nama : Intan Nas Nas Rabbiana
+NIM    : 11200930000010
+Kelas  : 2A- Sistem Informasi
+Matkul : Pemrograman lanjut
+*/
+package uts2;
+public class Person {
+   String Name;
+   int Age;
+   
+   void Name(String Name){
+       System.out.println("Name : " + Name);
+   }  
+    void Age(int Age){
+       System.out.println("Age : " + Age);
+   }
+   }
---
+++ uts/uts2/personberaksi.java
@@ -0,0 +1,19 @@
+/*Nama : Intan Nas Nas Rabbiana
+NIM    : 11200930000010
+Kelas  : 2A- Sistem Informasi
+Matkul : Pemrograman lanjut
+*/
+package uts2;
+
+public class personberaksi {
+    public static void main (String [] args){
+        
+        Person inputperson = new Person();
+        
+        inputperson.Name("Joe Smith");
+        inputperson.Age(24);
+        
+        inputperson.Name("Mary Sharp");   
+        inputperson.Age(52);
+    }
+    }
