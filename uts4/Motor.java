2958c61cba9c51e5c3b903e4e9aa5685f6f7514e
Intan Nas Nas Rabbiana <80866422+IntanNasNasRabbiana@users.noreply.github.com>
2021-04-22 09:23:51 +0700

    Add files via upload

---
+++ uts4/Motor.java
@@ -0,0 +1,16 @@
+/*Nama : Intan Nas Nas Rabbiana
+NIM    : 11200930000010
+Kelas  : 2A- Sistem Informasi
+Matkul : Pemrograman lanjut
+*/
+package uts4;
+
+public class Motor {
+    String tipe;
+    int tahun;
+    
+    void printMotor(){
+        System.out.println("Type Motor : "+ tipe);
+        System.out.println("Tahun : "+ tahun);
+    }
+} 
---
+++ uts4/Motorberaksi.java
@@ -0,0 +1,16 @@
+/*Nama : Intan Nas Nas Rabbiana
+NIM    : 11200930000010
+Kelas  : 2A- Sistem Informasi
+Matkul : Pemrograman lanjut
+*/
+package uts4;
+
+public class Motorberaksi {
+    public static void main(String[]args){
+        Motor inputmotor = new Motor();
+        
+        inputmotor.tipe = "Honda";
+        inputmotor.tahun = 2021;
+        inputmotor.printMotor();
+    }
+}
